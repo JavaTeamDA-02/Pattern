715f93e4f4a958467cf770473bfeb3ef752d842e
rudiuknastya <93525328+rudiuknastya@users.noreply.github.com>
2022-11-01 17:07:03 +0200

    Update WhiteTriangle.java

--- WhiteTriangle.java
+++ WhiteTriangle.java
@@ -2,24 +2,41 @@ package abstractFactory;
 
 public class WhiteTriangle implements Triangle {
 	private String color = "White";
-	private int A;
-	private int B;
-	private int C;
+	private double a;
+	private double b;
+	private double c;
 public	WhiteTriangle () {
-	A = 1 + (int)Math.random()*100;
-	B = 1 + (int)Math.random()*100;
-	C = 1 + (int)Math.random()*100;
+	a = 1 + Math.random()*100;
+	b = 1 + Math.random()*100;
+	c = 1 + Math.random()*100;
 }
+
 public boolean ifTriangle() {
-	if( (A+B)>C && (A+C)>B && (B+C)>A) {
+	if( (a+b)> c && (a+c)> b && (b+c)> a ) {
 		return true;
 	} else {
 		return false;
 	}
 	
 }
-
+public double getA () {
+	return a;
+}
+public double getB () {
+	return b;
+}
+public double getC () {
+	return c;
+}
 public String getColor () {
-	return color;
+		return color;
+	}
+
+public double getPerimetr () {
+	return a+b+c;
+}
+public double getSquare () {
+	double p = getPerimetr()/2;
+	return Math.sqrt(p*(p-a)*(p-b)*(p-c));
 }
 }

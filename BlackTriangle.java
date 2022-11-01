a29db404bc3aa4e42c909e427591b329b1a24ac2
rudiuknastya <93525328+rudiuknastya@users.noreply.github.com>
2022-11-01 17:05:58 +0200

    Update BlackTriangle.java

--- BlackTriangle.java
+++ BlackTriangle.java
@@ -2,23 +2,41 @@ package abstractFactory;
 
 public class BlackTriangle implements Triangle {
 	private String color = "Black";
-	private int A;
-	private int B;
-	private int C;
+	private double a;
+	private double b;
+	private double c;
 public	BlackTriangle () {
-	A = 1 + (int)Math.random()*100;
-	B = 1 + (int)Math.random()*100;
-	C = 1 + (int)Math.random()*100;
+	a = 1 + Math.random()*100;
+	b = 1 + Math.random()*100;
+	c = 1 + Math.random()*100;
 }
+
 public boolean ifTriangle() {
-	if( (A+B)>C && (A+C)>B && (B+C)>A ) {
+	if( (a+b)> c && (a+c)> b && (b+c)> a ) {
 		return true;
 	} else {
 		return false;
 	}
 	
 }
-	public String getColor () {
+public double getA () {
+	return a;
+}
+public double getB () {
+	return b;
+}
+public double getC () {
+	return c;
+}
+public String getColor () {
 		return color;
 	}
+
+public double getPerimetr () {
+	return a+b+c;
+}
+public double getSquare () {
+	double p = getPerimetr()/2;
+	return Math.sqrt(p*(p-a)*(p-b)*(p-c));
+}
 }
